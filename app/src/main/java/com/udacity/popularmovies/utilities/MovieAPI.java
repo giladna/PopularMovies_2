4a3d444899e5db2f4b824d12300c2e8d02419896
Gilad Nadav <gilad.nadav@kaltura.com>
2019-06-14 18:53:47 +0300

    Update MovieAPI.java

--- app/src/main/java/com/udacity/popularmovies/utilities/MovieAPI.java
+++ app/src/main/java/com/udacity/popularmovies/utilities/MovieAPI.java
@@ -21,14 +21,6 @@ import static com.udacity.popularmovies.utilities.NetworkUtils.PARAM_PAGE;
  */
 
 public interface MovieAPI {
-//https://api.themoviedb.org/3/movie/299537?api_key=8778f67f11a5a1dcb6ee731a56892416
-//https://api.themoviedb.org/3/movie/299537/videos?api_key=8778f67f11a5a1dcb6ee731a56892416
-// https://api.themoviedb.org/3/movie/420817/videos?api_key=8778f67f11a5a1dcb6ee731a56892416
-// https://api.themoviedb.org/3/movie/420817/reviews?api_key=8778f67f11a5a1dcb6ee731a56892416
-
-//https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=8778f67f11a5a1dcb6ee731a56892416
-//https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=8778f67f11a5a1dcb6ee731a56892416
-    //https://img.youtube.com/vi/9g5knnlF7Zo/0.jpg
     @GET("movie/popular")
     Call<DiscoverMoviesResponse<MovieMetadata>> getPopularMovies(@Query(API_KEY) String apiKey, @Query(PARAM_PAGE) int pageId);
 
